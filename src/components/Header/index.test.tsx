f14e7bb66ef06e1fe0b47b3dce0d073dc8040a8e
Joao Ghabriel <joaoghabriel02@gmail.com>
2023-08-02 21:32:13 -0300

    installed jest with rtl and created first test

--- src/components/Header/index.test.tsx
+++ src/components/Header/index.test.tsx
@@ -1,5 +1,24 @@
+import Header from ".";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { ThemeContextProvider } from "../../context/theme/ThemeContext";
+import { ThemeProvider } from "styled-components";
+import light from "../../styles/themes/light";
+
+const RenderHeader = () => {
+  render(
+    <ThemeContextProvider>
+      <ThemeProvider theme={light}>
+        <Header />
+      </ThemeProvider>
+    </ThemeContextProvider>
+  );
+};
 describe("testing header", () => {
   it("button should change the text to dark when toggled the first time", () => {
-    expect(1).toBe(1);
+    RenderHeader();
+    const button = screen.getByText("switch to dark");
+    fireEvent.click(button);
+    const text = screen.getByText("switch to light");
+    expect(text).toBeInTheDocument();
   });
 });
