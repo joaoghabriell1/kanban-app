9d05b3933bd37150ab4c541299e657e5e64cefe8
Joao Ghabriel <joaoghabriel02@gmail.com>
2023-09-10 00:11:59 -0300

    changed import syntax

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import ThemeContext from "./context/Theme/ThemeContext.tsx";
 import GlobalStyles from "./styles/GlobalStyles.ts";
 import { ThemeProvider } from "styled-components";
 import Routes from "./routes/Routes";
 import { useContext } from "react";
+import ThemeContext from "context/Theme/ThemeContext.tsx";
 
 function App() {
   const { currentTheme } = useContext(ThemeContext);
